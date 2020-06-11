84bd9a5740e9410fc34c4a4bf87fad9c808a4dfe
SloppyJack <312883327@qq.com>
2020-06-11 21:03:20 +0800

    解决Bug-使用@RestController标签（注：前端访问后端跨域会发送两个请求）

--- src/main/java/com/example/jianzhang/controller/v1/SpendCategoryController.java
+++ src/main/java/com/example/jianzhang/controller/v1/SpendCategoryController.java
@@ -3,10 +3,10 @@ package com.example.jianzhang.controller.v1;
 import com.example.jianzhang.model.SpendCategoryDO;
 import com.example.jianzhang.service.SpendCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -16,7 +16,7 @@ import java.util.List;
  * @description: com.example.jianzhang.controller.v1
  * @date: 2020/6/10 21:19
  **/
-@Controller
+@RestController
 @RequestMapping("/v1/spendCategory")
 @Validated
 public class SpendCategoryController {
