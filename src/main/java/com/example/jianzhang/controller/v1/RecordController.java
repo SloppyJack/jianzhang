6ad0afe348cacc6a735d939f4167dba5dc966824
SloppyJack <312883327@qq.com>
2020-06-12 23:14:40 +0800

    成功使用jwt获取当前登录的用户

--- src/main/java/com/example/jianzhang/controller/v1/RecordController.java
+++ src/main/java/com/example/jianzhang/controller/v1/RecordController.java
@@ -4,6 +4,8 @@ import com.example.jianzhang.dto.book.CreateOrUpdateBookDTO;
 import com.example.jianzhang.dto.record.CreateOrUpdateRecordDTO;
 import com.example.jianzhang.service.RecordService;
 import com.example.jianzhang.vo.CreatedVO;
+import io.github.talelin.core.annotation.GroupMeta;
+import io.github.talelin.core.annotation.LoginRequired;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,6 +34,8 @@ public class RecordController {
      *@author: edit by bin
      */
     @PostMapping("")
+    @GroupMeta(permission = "记录一笔账", module = "记账", mount = true)
+    @LoginRequired
     public CreatedVO createRecord(@RequestBody @Validated CreateOrUpdateRecordDTO validator) {
         recordService.createRecord(validator);
         return new CreatedVO(16);
