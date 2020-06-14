51d2981d91118bb8b19e59365ffa77513fc55607
SloppyJack <312883327@qq.com>
2020-06-14 21:54:29 +0800

    后端返回的日期格式化

--- src/main/java/com/example/jianzhang/model/RecordDO.java
+++ src/main/java/com/example/jianzhang/model/RecordDO.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
@@ -29,6 +30,7 @@ public class RecordDO {
 
     private String remarks;
 
+    @JsonFormat(pattern ="yyyy-MM-dd",timezone ="GMT+8")
     private Date occurTime;
 
     @JsonIgnore
