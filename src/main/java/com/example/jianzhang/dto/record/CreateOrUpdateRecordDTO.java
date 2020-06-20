b47694ccc6f496cef0beee5ef93dc7db933fdd7a
SloppyJack <312883327@qq.com>
2020-06-20 17:50:49 +0800

    接口某人使用下划线转换，去除@JsonProperty注释

--- src/main/java/com/example/jianzhang/dto/record/CreateOrUpdateRecordDTO.java
+++ src/main/java/com/example/jianzhang/dto/record/CreateOrUpdateRecordDTO.java
@@ -1,12 +1,8 @@
 package com.example.jianzhang.dto.record;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
@@ -17,21 +13,16 @@ import java.util.Date;
  * @date: 2020/6/7 20:28
  **/
 @Data
-@NoArgsConstructor
 public class CreateOrUpdateRecordDTO {
     @NotNull(message = "{record.spendCategory.not-empty}")
-    @JsonProperty(value = "spend_category")
     private Long spendCategory;
 
     @NotNull(message = "{record.amount.not-empty}")
-    @JsonProperty(value = "amount")
     private Double amount;
 
     @DateTimeFormat(pattern="yyyy-MM-dd hh:mm:ss")
-    @JsonProperty(value = "occur_time")
     private Date occurTime;
 
     // 备注
-    @JsonProperty(value = "remarks")
     private String remarks;
 }
